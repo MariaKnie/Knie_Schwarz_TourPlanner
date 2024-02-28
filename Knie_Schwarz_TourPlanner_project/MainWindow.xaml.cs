088429f3cbe2c63fd4ca52bfd4cd614ccdd19b81
MariaKnie <mariaknie@gmx.at>
2024-02-28 15:43:38 +0100

    pop up!

--- Knie_Schwarz_TourPlanner_project/MainWindow.xaml.cs
+++ Knie_Schwarz_TourPlanner_project/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Knie_Schwarz_TourPlanner_project
 {
@@ -19,9 +20,9 @@ namespace Knie_Schwarz_TourPlanner_project
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private string greetingText = "Hello";
-
         public string GreetingName { get; set; } = "World";
 
+        private DispatcherTimer timer;
         public string GreetingText
         {
             get => greetingText;
@@ -34,6 +35,8 @@ namespace Knie_Schwarz_TourPlanner_project
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;  // during initialization the engine registers itself to listen to PropertyChanged events
@@ -50,6 +53,8 @@ namespace Knie_Schwarz_TourPlanner_project
             Debug.WriteLine($"Button_Click GreetingName is {GreetingName}");
             GreetingText = $"Hello {GreetingName}!";
             Debug.WriteLine($"Button_Click GreetingText is {GreetingText}");
+
+            ShowPopup_Click(sender, e);
         }
 
         private void ButtonLogin_Click(object sender, RoutedEventArgs e)
@@ -57,6 +62,21 @@ namespace Knie_Schwarz_TourPlanner_project
             Debug.WriteLine($"Button_Click GreetingName is {GreetingName}");
             GreetingText = $"You have logged in, {GreetingName}!";
             Debug.WriteLine($"Button_Click GreetingText is {GreetingText}");
+
+            ShowPopup_Click(sender, e);
+        }
+
+        private void ShowPopup_Click(object sender, RoutedEventArgs e)
+        {
+            myPopup.IsOpen = true;
+            timer.Interval = TimeSpan.FromSeconds(3); // Adjust duration here
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            myPopup.IsOpen = false;
+            timer.Stop();
         }
     }
 }
