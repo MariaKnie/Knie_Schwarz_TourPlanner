52641f18807088241993f61129ee87948669177d
MariaKnie <mariaknie@gmx.at>
2024-03-16 17:56:09 +0100

    Added Unit Test Project

---
+++ Knie_Schwarz_TourPlanner.Tests/ViewModels/ViewModelTests.cs
@@ -0,0 +1,45 @@
+using Knie_Schwarz_TourPlanner_project.ViewModels;
+
+namespace TaskPlannerA.Tests.ViewModels
+{
+    public class ViewModelTests
+    {
+        //private EditorViewModel edtVM;
+        private MainViewModel mainVM;
+
+        [SetUp]
+        public void Setup()
+        {
+            //edtVM = new EditorViewModel();
+            //mainVM = new MainViewModel(edtVM);
+            mainVM = new MainViewModel;
+        }
+
+        [Test]
+        public void TestTaskPlanner_ShouldContainInitialList()
+        {
+            // Arrange --> in Setup
+            // Act
+            int expectedCount = 4;
+            int actualCount = mainVM.Tourlist.Count;
+            // Assert
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void TestTaskPlanner_AddTask_ShouldUpdateList()
+        {
+            // Arrange
+            int lastTaskCount = mainVM.Tasks.Count;
+            int expectedTaskCount = lastTaskCount + 1;
+            string expectedTaskName = "Test Task";
+            // Act & Assert
+            edtVM.NewTaskName = expectedTaskName;
+            edtVM.AddTaskCommand.Execute(null);
+            int actualTaskCount = mainVM.Tasks.Count;
+            Assert.That(actualTaskCount, Is.EqualTo(expectedTaskCount));
+            string actualTaskName = mainVM.Tasks[actualTaskCount - 1].Name;
+            Assert.That(actualTaskName, Is.EqualTo(expectedTaskName));
+        }
+    }
+}
